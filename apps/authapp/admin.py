ca07a6a4b54511c0bda678eb998e9e01af968b58
= <walidpiano@yahoo.com>
2020-07-25 03:49:18 +0200

    update: search users admin

--- apps/authapp/admin.py
+++ apps/authapp/admin.py
@@ -5,7 +5,8 @@ from .models import User
 # Register your models here.
 class AdminUser(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'phone')
-    list_filter = ('email',)
+    # list_filter = ('email',)
+    search_fields = ('username', 'email', 'phone',)
 
 
 admin.site.register(User, AdminUser)
